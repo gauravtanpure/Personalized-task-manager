2a0d425fd8c902ad5de5d3f92bc857ebb566e88e
Gaurav Tanpure <gauravtanpure999@gmail.com>
2025-03-12 23:27:56 +0530

    errors

--- server/config.js
+++ server/config.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb+srv://gaurav:123@task-manager-cluster.pujz9.mongodb.net/?retryWrites=true&w=majority&appName=task-manager-cluster", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log("MongoDB Connected...");
   } catch (error) {
