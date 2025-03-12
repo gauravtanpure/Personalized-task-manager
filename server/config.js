7e23c1039a0dbafeaac8915537c9ef2d315fbc81
Gaurav Tanpure <147535321+gauravtanpure@users.noreply.github.com>
2025-03-13 00:27:30 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://<username>:<password>@task-manager-cluster.pujz9.mongodb.net/?retryWrites=true&w=majority&appName=task-manager-cluster", {
+    await mongoose.connect(process.env.MONGO_URI, {
     });
     console.log("MongoDB Connected...");
   } catch (error) {
